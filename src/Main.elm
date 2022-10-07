7e707c8f89266d76b82dbdf078373f8497064905
Jesse Warden <jesterxl@jessewarden.com>
2022-10-07 15:57:59 -0400

    dynamic data

--- src/Main.elm
+++ src/Main.elm
@@ -185,114 +185,127 @@ view model =
             div [class "m-auto pt-4 pb-4 z-10"][img [src "images/Alto_logo.png", class "w-[50px] h-[14px]"][]]
             , dots model.screen
         ]
-        , case model.screen of
-            Loading ->
-                div [][text "Loading..."]
-            ErrorScreen ->
-                div [][
-                    text "We failed to load your trip data. Please try again in a few seconds."
-                    , button [][ text "Retry"] 
-                ]
-            TripScreen ->
-                div [class "flex grow flex-col"][
-                    h1 [class "font-optima text-4xl pt-10 pb-10"][text "Your Trip"]
-                    , h2 [class "font-pxgrotesk text-7xl"][
-                        text "5:39 "
-                        , span [class "text-3xl uppercase"][text "pm"]
-                    ]
-                    , p [class "pb-8 text-alto-base text-alto-primary"][text "Estimated arrival at DFW Int'l Airport - Terminal E"]
-                    , div [class "flex flex-row gap-8 pb-12"][
-                        div [class "flex flex-col basis-1/3 border-t-2 border-t-solid border-t-alto-line"][
-                            p [class "text-alto-title text-alto-primary opacity-75"][text "Estimated Fare:"]
-                            , p [class "flex flex-row items-center gap-1 text-alto-base font-bold opacity-60"][text "$65 - $75", span [][img [src "images/Info_icon.png", class "w-[13px] h-[13px]"][]]]
+        , case model.tripData of
+            Nothing ->
+                case model.screen of
+                    Loading ->
+                        div [][text "Loading..."]
+                    ErrorScreen ->
+                        div [][
+                            text "We failed to load your trip data. Please try again in a few seconds."
+                            , button [][ text "Retry"] 
                         ]
-                        , div [class "flex flex-col basis-1/3 border-t-2 border-t-solid border-t-alto-line"][
-                            p [class "text-alto-title text-alto-primary opacity-75"][text "Passengers:"]
-                            , p [class "text-alto-base font-bold opacity-60"][text "1 - 5"]
+                    _ ->
+                        div [][text "Reload."]
+            Just tripData ->
+                case model.screen of
+                    Loading ->
+                        div [][text "Loading..."]
+                    ErrorScreen ->
+                        div [][
+                            text "We failed to load your trip data. Please try again in a few seconds."
+                            , button [][ text "Retry"] 
                         ]
-                        , div [class "flex flex-col basis-1/3 border-t-2 border-t-solid border-t-alto-line"][
-                            p[class "text-alto-title text-alto-primary opacity-75"][text "Payment:"]
-                            , p [class "text-alto-base font-bold opacity-60"][text "Amex01"]
+                    TripScreen ->
+                        div [class "flex grow flex-col"][
+                            h1 [class "font-optima text-4xl pt-10 pb-10"][text "Your Trip"]
+                            , h2 [class "font-pxgrotesk text-7xl"][
+                                text "5:39 "
+                                , span [class "text-3xl uppercase"][text "pm"]
+                            ]
+                            , p [class "pb-8 text-alto-base text-alto-primary"][text ("Estimated arrival at " ++ (tripData.trip.dropoff.name |> Maybe.withDefault "???"))]
+                            , div [class "flex flex-row gap-8 pb-12"][
+                                div [class "flex flex-col basis-1/3 border-t-2 border-t-solid border-t-alto-line"][
+                                    p [class "text-alto-title text-alto-primary opacity-75"][text "Estimated Fare:"]
+                                    , p [class "flex flex-row items-center gap-1 text-alto-base font-bold opacity-60"][text "$65 - $75", span [][img [src "images/Info_icon.png", class "w-[13px] h-[13px]"][]]]
+                                ]
+                                , div [class "flex flex-col basis-1/3 border-t-2 border-t-solid border-t-alto-line"][
+                                    p [class "text-alto-title text-alto-primary opacity-75"][text "Passengers:"]
+                                    , p [class "text-alto-base font-bold opacity-60"][text "1 - 5"]
+                                ]
+                                , div [class "flex flex-col basis-1/3 border-t-2 border-t-solid border-t-alto-line"][
+                                    p[class "text-alto-title text-alto-primary opacity-75"][text "Payment:"]
+                                    , p [class "text-alto-base font-bold opacity-60"][text "Amex01"]
+                                ]
+                            ]
+                            , div [class "pb-2 text-alto-base text-alto-primary opacity-75"][
+                                    p [][text "449 Flora St."]
+                                    , p [][text "Dallas, Texas 75201"]
+                                ]
+                            , div [class "pt-2 pb-2 border-t-2 border-t-solid border-t-alto-line"][]
+                            , div [class "pb-4 text-alto-base text-alto-primary font-bold opacity-75"][
+                                p[][text "DFW International Airport"]
+                                , p[][text "American Airlines Terminal E"]
+                                , p[][text "Irving, Texas 75261"]
+                            ]
+                            , div [class "flex flex-row gap-4 items-center text-alto-base text-alto-primary opacity-75"][
+                                p[][text "Can you drop me off at AA International Bag Drop please?"]
+                                , img [src "images/Edit_icon.png", class "w-[10px] h-[10px]"][]
+                            ]
+                            , div [class "grow"][]
+                            , button [ class "p-4 border-2 border-solid border-alto-line"][
+                                span [class "uppercase text-alto-base font-semibold text-alto-primary opacity-20"][text "Cancel Trip"]
+                            ]
+                        
                         ]
-                    ]
-                    , div [class "pb-2 text-alto-base text-alto-primary opacity-75"][
-                            p [][text "449 Flora St."]
-                            , p [][text "Dallas, Texas 75201"]
+                    DriverScreen ->
+                        div [class "flex grow flex-col"][
+                            img [class "object-none object-[50%_39%]", src "images/Driver_photo.png"][]
+                            , h1 [class "font-pxgrotesk text-alto-title tracking-widest uppercase text-alto-dark pt-8 pb-8"][text "Your Driver"]
+                            , h2 [class "font-pxgrotesklight text-7xl tracking-tighter"][text "Steph"]
+                            , div [][
+                                div [class "pt-2 pb-2 border-t-2 border-t-solid border-t-alto-line"][]
+                                , p [class "text-alto-title tracking-tight text-alto-primary opacity-75"][text "Steph Festiculma is a graduate of Parsons New School in New York and fluent in Portugeuse, Spanish and English. Steph has been driving with Alto since 2018."]
+                            ]
+                            , div [class "grow"][]
+                            , button [ class "mt-4 p-4 border-2 border-solid border-alto-line w-screen"][
+                                span [class "uppercase text-alto-base font-semibold text-alto-primary opacity-20"][text "Contact Driver"]
+                            ]
+                        
                         ]
-                    , div [class "pt-2 pb-2 border-t-2 border-t-solid border-t-alto-line"][]
-                    , div [class "pb-4 text-alto-base text-alto-primary font-bold opacity-75"][
-                        p[][text "DFW International Airport"]
-                        , p[][text "American Airlines Terminal E"]
-                        , p[][text "Irving, Texas 75261"]
-                    ]
-                    , div [class "flex flex-row gap-4 items-center text-alto-base text-alto-primary opacity-75"][
-                        p[][text "Can you drop me off at AA International Bag Drop please?"]
-                        , img [src "images/Edit_icon.png", class "w-[10px] h-[10px]"][]
-                    ]
-                    , div [class "grow"][]
-                    , button [ class "p-4 border-2 border-solid border-alto-line"][
-                        span [class "uppercase text-alto-base font-semibold text-alto-primary opacity-20"][text "Cancel Trip"]
-                    ]
-                
-                ]
-            DriverScreen ->
-                div [class "flex grow flex-col"][
-                    img [class "object-none object-[50%_39%]", src "images/Driver_photo.png"][]
-                    , h1 [class "font-pxgrotesk text-alto-title tracking-widest uppercase text-alto-dark pt-8 pb-8"][text "Your Driver"]
-                    , h2 [class "font-pxgrotesklight text-7xl tracking-tighter"][text "Steph"]
-                    , div [][
-                        div [class "pt-2 pb-2 border-t-2 border-t-solid border-t-alto-line"][]
-                        , p [class "text-alto-title tracking-tight text-alto-primary opacity-75"][text "Steph Festiculma is a graduate of Parsons New School in New York and fluent in Portugeuse, Spanish and English. Steph has been driving with Alto since 2018."]
-                    ]
-                    , div [class "grow"][]
-                    , button [ class "mt-4 p-4 border-2 border-solid border-alto-line w-screen"][
-                        span [class "uppercase text-alto-base font-semibold text-alto-primary opacity-20"][text "Contact Driver"]
-                    ]
-                
-                ]
-            VehicleScreen ->
-                div [class "flex grow flex-col"][
-                    img [class "object-none object-[50%_39%]", src "images/Vehicle_photo.png"][]
-                    , h1 [class "font-pxgrotesk text-alto-title tracking-widest uppercase text-alto-dark pt-8 pb-8"][text "Your Vehicle"]
-                    , h2 [class "font-pxgrotesklight text-7xl tracking-tighter"][text "Alto 09"]
-                    , div [class "flex flex-row w-screen gap-8 pb-12 pt-8"][
-                        div [class "flex flex-col basis-1/2 border-t-2 border-t-solid border-t-alto-line"][
-                            p [class "text-alto-title text-alto-primary opacity-75"][text "Make / Model"]
-                            , p [class "flex flex-row items-center gap-1 text-alto-base font-bold opacity-60"][text "2019 Volkswagen Atlas"]
+                    VehicleScreen ->
+                        div [class "flex grow flex-col"][
+                            img [class "object-none object-[50%_39%]", src "images/Vehicle_photo.png"][]
+                            , h1 [class "font-pxgrotesk text-alto-title tracking-widest uppercase text-alto-dark pt-8 pb-8"][text "Your Vehicle"]
+                            , h2 [class "font-pxgrotesklight text-7xl tracking-tighter"][text "Alto 09"]
+                            , div [class "flex flex-row w-screen gap-8 pb-12 pt-8"][
+                                div [class "flex flex-col basis-1/2 border-t-2 border-t-solid border-t-alto-line"][
+                                    p [class "text-alto-title text-alto-primary opacity-75"][text "Make / Model"]
+                                    , p [class "flex flex-row items-center gap-1 text-alto-base font-bold opacity-60"][text "2019 Volkswagen Atlas"]
+                                ]
+                                , div [class "flex flex-col basis-1/2 border-t-2 border-t-solid border-t-alto-line"][
+                                    p [class "text-alto-title text-alto-primary opacity-75"][text "Color"]
+                                    , p [class "text-alto-base font-bold opacity-60"][text "Pure White"]
+                                ]
+                            ]
+                            , div [class "grow"][]
+                            , button [ class "mt-4 p-4 border-2 border-solid border-alto-line w-screen"][
+                                span [class "uppercase text-alto-base font-semibold text-alto-primary opacity-20"][text "Identify Vehicle"]
+                            ]
+                        
                         ]
-                        , div [class "flex flex-col basis-1/2 border-t-2 border-t-solid border-t-alto-line"][
-                            p [class "text-alto-title text-alto-primary opacity-75"][text "Color"]
-                            , p [class "text-alto-base font-bold opacity-60"][text "Pure White"]
+                    VibeScreen ->
+                        div [class "flex grow flex-col"][
+                            -- TODO/FIXME: this works with the whole absolute + top-4 thing,
+                            -- but the rest of the flex content gets confused
+                            img [class "vibeMask top-4 absolute", src "images/Map_overview.png"][]
+                            , img [class "top-60 right-4 absolute", src "images/Map_icon.png"][]
+                            , h1 [class "pt-[250px] font-pxgrotesk text-alto-title tracking-widest uppercase text-alto-dark pt-8 pb-8"][text "Your Trip"]
+                            , h2 [class "font-pxgrotesklight text-7xl"][
+                                text "5:39 "
+                                , span [class "text-3xl uppercase"][text "pm"]
+                            ]
+                            , p [class "pb-8 text-alto-base text-alto-primary"][text "Estimated arrival at DFW Int'l Airport - Terminal E"]
+                            , div [class "flex flex-col w-screen pb-12 pt-8 border-t-2 border-t-solid border-t-alto-line"][
+                                p [class "text-alto-title text-alto-primary opacity-75"][text "Current Vibe"]
+                                , p [class "flex flex-row items-center gap-1 text-alto-base font-bold opacity-60"][text "Vaporwave Beats"]
+                            ]
+                            , div [class "grow"][]
+                            , button [ class "mt-4 p-4 border-2 border-solid border-alto-line w-screen bg-alto-dark" ][
+                                span [class "uppercase text-alto-base font-semibold text-white"][text "Change Vehicle Vibe"]
+                            ]
+                        
                         ]
-                    ]
-                    , div [class "grow"][]
-                    , button [ class "mt-4 p-4 border-2 border-solid border-alto-line w-screen"][
-                        span [class "uppercase text-alto-base font-semibold text-alto-primary opacity-20"][text "Identify Vehicle"]
-                    ]
-                
-                ]
-            VibeScreen ->
-                div [class "flex grow flex-col"][
-                    -- TODO/FIXME: this works with the whole absolute + top-4 thing,
-                    -- but the rest of the flex content gets confused
-                    img [class "vibeMask top-4 absolute", src "images/Map_overview.png"][]
-                    , img [class "top-60 right-4 absolute", src "images/Map_icon.png"][]
-                    , h1 [class "pt-[250px] font-pxgrotesk text-alto-title tracking-widest uppercase text-alto-dark pt-8 pb-8"][text "Your Trip"]
-                    , h2 [class "font-pxgrotesklight text-7xl"][
-                        text "5:39 "
-                        , span [class "text-3xl uppercase"][text "pm"]
-                    ]
-                    , p [class "pb-8 text-alto-base text-alto-primary"][text "Estimated arrival at DFW Int'l Airport - Terminal E"]
-                    , div [class "flex flex-col w-screen pb-12 pt-8 border-t-2 border-t-solid border-t-alto-line"][
-                        p [class "text-alto-title text-alto-primary opacity-75"][text "Current Vibe"]
-                        , p [class "flex flex-row items-center gap-1 text-alto-base font-bold opacity-60"][text "Vaporwave Beats"]
-                    ]
-                    , div [class "grow"][]
-                    , button [ class "mt-4 p-4 border-2 border-solid border-alto-line w-screen bg-alto-dark" ][
-                        span [class "uppercase text-alto-base font-semibold text-white"][text "Change Vehicle Vibe"]
-                    ]
-                
-                ]
 
         , div [ class "mt-6 pt-2 flex flex-row border-t-2 border-t-solid border-t-alto-line"][
             div [class "m-auto w-[24px] h-[24px]"][img [src "images/Profile_icon.png"][]]
