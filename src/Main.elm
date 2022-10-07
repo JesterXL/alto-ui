2b4c23e512c5ade8370d862984b7513161dff28a
Jesse Warden <jesterxl@jessewarden.com>
2022-10-06 20:35:57 -0400

    let's just do screens, no routing for now

--- src/Main.elm
+++ src/Main.elm
@@ -3,85 +3,51 @@ module Main exposing (main)
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
-import Url
 import Json.Decode as JD
-import Browser.Navigation as Nav
-import Url.Parser as UrlParser
 
-type alias Model =
-    { key : Nav.Key
-    , url : Url.Url
-    , route : Route }
+type alias Model = 
+    { screen : Screen }
 
-type Route
+type Screen
     = Loading
-    | NotFound
     | Trip
     | Driver
     | Vehicle
-    | Destination
-
-routeParser : UrlParser.Parser (Route -> a) a
-routeParser =
-    UrlParser.oneOf
-        [ UrlParser.map Loading UrlParser.top
-        , UrlParser.map Trip (UrlParser.s "trip")
-        , UrlParser.map Driver (UrlParser.s "driver")
-        , UrlParser.map Vehicle (UrlParser.s "vehicle")
-        , UrlParser.map Destination (UrlParser.s "destination")
-        ]
+    | Vibe
 
 
-parseRoute : Url.Url -> Route
-parseRoute url =
-    UrlParser.parse routeParser url
-        |> Maybe.withDefault NotFound
-
-initialModel : Nav.Key -> Url.Url -> Model
-
-initialModel key url =
-    { key = key 
-    , url = url
-    , route = parseRoute url }
+initialModel : Model
+initialModel =
+    { screen = Loading }
 
 type Msg
-    = LinkClicked Browser.UrlRequest
-    | UrlChanged Url.Url
+    = Noop
 
 
 update : Msg -> Model -> ( Model, Cmd Msg)
 update msg model =
     case msg of
-        LinkClicked _ ->
+        Noop ->
             ( model, Cmd.none )
-        UrlChanged url ->
-            ( { model | url = url, route = parseRoute url }, Cmd.none )
 
-
-view : Model -> Browser.Document Msg
+view : Model -> Html Msg 
 view model =
-    { title = "Alto"
-    , body = [ div []
-        [ case model.route of
-            Loading ->
-                div [][text "Loading..."]
-            NotFound ->
-                div [][text "404 Not Found"]
-            Trip ->
-                div [][text "Trip"]
-            Driver ->
-                div [][text "Driver"]
-            Vehicle ->
-                div [][text "Vehicle"]
-            Destination ->
-                div [][text "Destination"]
-        ]
-    ] }
+    case model.screen of
+        Loading ->
+            div [][text "Loading..."]
+        Trip ->
+            div [][text "Trip"]
+        Driver ->
+            div [][text "Driver"]
+        Vehicle ->
+            div [][text "Vehicle"]
+        Vibe ->
+            div [][text "Vibe"]
 
 
-init : JD.Value -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
-init _ url key =
-    ( initialModel key url, Cmd.none )
+init : JD.Value -> ( Model, Cmd Msg )
+init _ =
+    ( initialModel, Cmd.none )
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
@@ -89,11 +55,9 @@ subscriptions _ =
 
 main : Program JD.Value Model Msg
 main =
-    Browser.application
+    Browser.element
         { init = init
         , view = view
         , update = update
         , subscriptions = subscriptions
-        , onUrlRequest = LinkClicked
-        , onUrlChange = UrlChanged
         }
